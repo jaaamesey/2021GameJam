e842460f452981ccd469cd56bce9d14d75395d67
James <jameskarlsson1@gmail.com>
2021-01-04 01:29:41 +1100

    Fixed standalone levels

--- scn/level_fragment.gd
+++ scn/level_fragment.gd
@@ -8,9 +8,10 @@ onready var tilemap_rect_size: Vector2 = tilemap.get_used_rect().size
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	if owner == null:
+		return
 	remove_child($GUIDES)
 	remove_child($player)
-	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
