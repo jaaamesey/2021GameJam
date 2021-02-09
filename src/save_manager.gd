2111b9bccfa4b98b8bf6af69d5ed9f2fa53c07ff
James Karlsson <jameskarlsson1@gmail.com>
2021-02-09 18:14:32 +1100

    More responsible file handling

--- src/save_manager.gd
+++ src/save_manager.gd
@@ -1,14 +1,19 @@
 extends Node
 
+var high_score_location := "user://high_score.sav"
 func get_high_score() -> float:
 	var file := File.new()
-	file.open("user://high_score.sav", File.READ)
+	if !file.file_exists(high_score_location):
+		file.close()
+		return 0.0
+		
+	file.open(high_score_location, File.READ)
 	var score := float(file.get_line())
 	file.close()
 	return score
 
 func save_high_score(score: float):
 	var file := File.new()
-	file.open("user://high_score.sav", File.WRITE)
+	file.open(high_score_location, File.WRITE)
 	file.store_line(str(score))
 	file.close()
